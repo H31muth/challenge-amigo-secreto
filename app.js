1bf2df90bf17d749c8bfe3b6400acf4fcae585db
Helmut Ruge <hrug3@icloud.com>
2025-01-28 23:28:19 -0300

    Actualiza el app

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+function agregarAmigo(){
+   let nombreAmigo = document.getElementById('amigo'); 
+   console.log(nombreAmigo);
+   if (amigo===''){
+    alert('Favor, inserte un nombre válido.');
+    return;
+   }
+}
+
