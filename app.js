142a483ac79fd95b7e2f9de2935a8433ade318ff
Helmut Ruge <hrug3@icloud.com>
2025-01-31 20:35:54 -0300

    Nuevas modificaciones

--- app.js
+++ app.js
@@ -18,6 +18,26 @@ function agregarAmigo(){   // función para agregar un amigo
    actualizarListaAmigos():     // se llama a la función para actualizar la lista de amigos
 }
 function actualizarListaAmigos(){    // función para actualizar lista de amigos
+   let listaAmigo = document.getElementById("listaAmigos");
+   listaAmigo.innerHTML = "";   // limpia la lista 
 
+   for (let i = 0; i < amigos.length; i++) { 			// Recorre el array usando un bucle for
+      let li = document.createElement("li"); 			// Crea un nuevo elemento de lista
+      li.textContent = amigos[i]; 				// Asigna el nombre del amigo al elemento de lista
+      listaAmigo.appendChild(li); 				// Agrega el elemento a la lista
+  }
+
+}
+function sortearAmigo() {
+   if (amigos.length === 0) { 				// Verifica si hay amigos en la lista antes de sortear
+       alert("No hay amigos en la lista para sortear.");
+       return;
+   }
+   
+   let indiceAleatorio = Math.floor(Math.random() * amigos.length);	// Genera un índice aleatorio dentro del rango de la lista
+   let amigoSorteado = amigos[indiceAleatorio]; 			// Obtiene el amigo sorteado usando el índice aleatorio
+   
+   let resultado = document.getElementById("resultado"); 		// Captura el elemento donde se mostrará el resultado
+   resultado.innerHTML = `<li>Amigo Secreto: <strong>${amigoSorteado}</strong></li>`;       // Muestra el resultado en la interfaz
 }
 
